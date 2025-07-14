2c86054ec9ea444a577b0dd0f6caad83a046eaf0
Muhammadali <rahimov.dev1@gmail.com>
2025-07-14 13:29:17 +0500

    second commit

--- index.js
+++ index.js
@@ -8,10 +8,6 @@ const File = require('./models/File');  // <-- MUHIM!!! Modelni chaqirish
 
 
 const app = express();
-app.use(cors({
-  origin: 'http://localhost:3001',
-  credentials: true
-}));
 app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
